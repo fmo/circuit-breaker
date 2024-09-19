b1a280377337e863c8ee73f377c18350284adeb7
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-09-19 21:26:20 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fmo/circuit-breaker
+
+go 1.22
---
+++ middleware/circuit_breaker.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "context"
+
+func CircuitBreakerClientInterceptor(cb *gobreaker.CircuitBreaker) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		_, cbErr := cb.Execute(func() (interface{}, error) {
+			err := invoker(ctx, method, req, reply, cc, opts...)
+			if err != nil {
+				return nil, err
+			}
+
+			return nil, nil
+		})
+		return cbErr
+	}
+}
